8a2606273c2637f1f9d4de88afcd6d0ce46ed37b
ScreAzoe <castillorealberto@live.com>
2020-04-13 20:37:54 -0500

    test_stage2

--- CompiladorXenón/test/parser_test.exs
+++ CompiladorXenón/test/parser_test.exs
@@ -157,4 +157,258 @@ defmodule ParserTest do
       right_node: nil,
       value: nil}
   end
+
+
+#UNARIOS TO PASS
+
+test "BIT WISE" do
+  ast= Lexer.lexing("int main() {
+  return !12;
+  }")
+  assert Parser.parse_program(ast) ==
+  %AST{
+    left_node: %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: nil,
+            node_name: :constant,
+            right_node: nil,
+            value: 12
+          },
+          node_name: :unary,
+          right_node: nil,
+          value: :logicalNeg
+        },
+        node_name: :return,
+        right_node: nil,
+        value: :return
+      },
+      node_name: :function,
+      right_node: nil,
+      value: :main
+    },
+    node_name: :program,
+    right_node: nil,
+    value: nil
+  }
+  end
+
+  test "BIT WISE ZERO" do
+    ast= Lexer.lexing("int main() {
+      return ~0;
+      }")
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: nil,
+              node_name: :constant,
+              right_node: nil,
+              value: 0
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :bitWise
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+  test "NEG" do
+    ast= Lexer.lexing("int main() {
+      return -5;
+      }")
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: nil,
+              node_name: :constant,
+              right_node: nil,
+              value: 5
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :negation
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+  test "nested_ops" do
+    ast= Lexer.lexing("int main() {
+    return !-3;
+    }")
+
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: %AST{
+                left_node: nil,
+                node_name: :constant,
+                right_node: nil,
+                value: 3
+              },
+              node_name: :unary,
+              right_node: nil,
+              value: :negation
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :logicalNeg
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+  test "nested_ops_2" do
+    ast= Lexer.lexing("int main() {
+    return -~0;
+    }")
+
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: %AST{
+                left_node: nil,
+                node_name: :constant,
+                right_node: nil,
+                value: 0
+              },
+              node_name: :unary,
+              right_node: nil,
+              value: :bitWise
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :negation
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+  test "not five" do
+    ast= Lexer.lexing("int main() {
+    return !5;
+    }")
+
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: nil,
+              node_name: :constant,
+              right_node: nil,
+              value: 5
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :logicalNeg
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+  test "not zero" do
+    ast= Lexer.lexing("int main() {
+    return !0;
+    }")
+
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: nil,
+              node_name: :constant,
+              right_node: nil,
+              value: 0
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :logicalNeg
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+
+#OPERADORES UNARIO INVALIDOS
+
+
+
 end
